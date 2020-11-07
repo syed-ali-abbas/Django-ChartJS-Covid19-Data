6a15d18cab2da7986284526c418beb0530a90e06
SYED ALI ABBAS <syedaliabbasshahmajeed@gmail.com>
2020-11-07 12:53:48 +0500

    graph color changed and world map added from highchart script

--- chartapp/views.py
+++ chartapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import pandas as pd
 
+json_data = pd.read_json('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/world-population-density.json')
+
 def ChartJS(request):
     data = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
     total_count = data[data.columns[-1]].sum()
@@ -10,12 +12,29 @@ def ChartJS(request):
     temp = temp.sort_values(by='Values', ascending=False)
     country_names = temp['Country/Region'].tolist()
     people_effected = temp['Values'].tolist()
+    map_data=WorldMap(country_names,temp)
     context = {
         'total_count':total_count,
          'country_names':country_names,
-        'people_effected':people_effected
+        'people_effected':people_effected,
+        'map_data':map_data
     }
-
-
-
     return render(request,'index.html', context)
+
+
+
+def WorldMap(country_names,temp):
+    dataForMap = []
+    for i in country_names:
+        try:
+            temp_map = json_data[json_data['name'] == i]
+            t = {}
+            t["code3"] = list(temp_map['code3'].values)[0]
+            t["name"] = i
+            t["value"] = temp[temp['Country/Region'] == i]['Values'].sum()
+            t["code"] = list(temp_map['code'].values)[0]
+            dataForMap.append(t)
+        except:
+            pass
+    return dataForMap
+
